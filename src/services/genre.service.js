d7100f50c6696d80180c16b19139793784a7c507
adam-cowley <adam@adamcowley.co.uk>
2021-11-11 22:45:59 +0000

    Completed lesson: Finding Genre Details

--- src/services/genre.service.js
+++ src/services/genre.service.js
@@ -78,12 +78,38 @@ export default class GenreService {
    */
   // tag::find[]
   async find(name) {
-    // TODO: Open a new session
-    // TODO: Get Genre information from the database
-    // TODO: Throw a 404 Error if the genre is not found
-    // TODO: Close the session
+    // Open a new Session
+    const session = this.driver.session()
 
-    return genres.find(genre => genre.name === name)
+    // Get Genre information from the database
+    const res = await session.readTransaction(tx => tx.run(`
+      MATCH (g:Genre {name: $name})<-[:IN_GENRE]-(m:Movie)
+      WHERE exists(m.imdbRating) AND exists(m.poster) AND g.name <> '(no genres listed)'
+      WITH g, m
+      ORDER BY m.imdbRating DESC
+
+      WITH g, head(collect(m)) AS movie
+
+      RETURN g {
+        link: '/genres/'+ g.name,
+        .name,
+        movies: size((g)<-[:IN_GENRE]-()),
+        poster: movie.poster
+      } AS genre
+    `, { name }))
+
+    // Throw a 404 Error if the genre is not found
+    if ( res.records.length === 0 ) {
+        throw new NotFoundError(`Could not find a genre with the name '${name}'`)
+    }
+
+    // Close the session
+    await session.close()
+
+    // Return results
+    const [ row ] = res.records
+
+    return toNativeTypes(row.get('genre'))
   }
   // end::find[]
 
